71dc7daea013936120eafd03168b8a7700f098d4
Derrick Mwiti <mwitiderrick@gmail.com>
2018-01-20 19:23:48 +0000

    Bitcoin prices showing

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,29 @@
 import express from 'express';
-
+import request from 'request';
 const router = express.Router();
 
-/* GET index page. */
 router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Express'
-  });
-});
+request('https://api.coinmarketcap.com/v1/ticker/bitcoin',function (error, response, exchange1) {
+	if (!error && response.statusCode == 200) {
+			var obj1 = JSON.parse(exchange1)
+			}
+	request('https://api.coindesk.com/v1/bpi/currentprice.json', function (error, response, exchange2) {
+	if (!error && response.statusCode == 200) {
+		var obj2 = JSON.parse(exchange2);
+		}
+	request('https://blockchain.info/ticker', function (error, response, exchange3) {
+	if (!error && response.statusCode == 200) {
+		var obj3 = JSON.parse(exchange3);
+		res.render('index', {
+		exchange1: obj1,
+		exchange2: obj2,
+		exchange3: obj3
+		});
+		}
+
+	});
+			});
+				});  
+					});
 
 export default router;
