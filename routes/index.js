ac30eca7fbb751c0e7c5373658904383226a7981
Derrick Mwiti <mwitiderrick@gmail.com>
2018-01-24 21:34:13 +0000

    Aunthetication

--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,67 @@
 import express from 'express';
-import request from 'request';
 const router = express.Router();
 var bcrypt = require('bcrypt');
 var db = require('../db');
 var session = require('express-session');
 
 router.use(session({
-    key: 'email',
-    secret: 'ndnsfknskfskfbslkf',
+    key: 'useremail',
+    secret: 'jbknlnklnklbl khlk hli',
     resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
+    saveUninitialized: false
 }));
 
-var sessionChecker = (req, res) => {
-    if (req.session.email) {
+let sessionChecker = (req, res, next) => {
+    if (req.session.useremail) {
         res.redirect('/');
     } else {
-        res.redirect('/login')
+        next()
     }    
 };
 
-router.get('/', sessionChecker, (req, res) => {
+router.get('/',sessionChecker, (req, res) => {
+    
     res.render('index');
 });
 
-router.get('/signup', sessionChecker, (req, res) => {
+router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
 
 router.post('/signup', (req, res) => {
     let [email, username] = [req.body.email, req.body.username]
-     let hashpassword = bcrypt.hashSync(req.body.password, 10);
+    let hashpassword = bcrypt.hashSync(req.body.password, 10);
 
     db.query('INSERT INTO users (username,email,password) VALUES($1,$2,$3)',[username,email,hashpassword],(err,resp)=>{
-    if(err){
-        res.render('signup');
-    }else{
-        req.session.email = email;
-        res.render('index');
-    }     
+    // if(err){
+    //     res.redirect('/signup');
+    // }else{
+        res.redirect('/login');
+    // }     
 }); 
 });
 
+router.get('/login', (req, res) => { 
+    res.render('login');
+});
+ 
 router.post('/login', (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
-    db.query('SELECT * FROM users WHERE email=$1',[email],(err,res)=>{
-        if(err){
-            console.log('user doesnt exist, duh',err);
-        }else{
-           if(bcrypt.compareSync(password, resrows[0].password)){
-            res.render('index');
-           }
+    let hashed = bcrypt.hashSync(password, 10);
+    db.query('SELECT * FROM users WHERE email=$1',[email])
+    .then(resp => {
+        console.log('is in db'+resp.rows[0].password)
+        if (resp.rowCount && bcrypt.compareSync(resp.rows[0].password,hashed)) {
 
+            req.session.useremail = email;
+            res.redirect('/');
         }
     })
+    .catch(err => console.log(err.stack));
 });
 
-
-router.get('/login', sessionChecker, (req, res) => {
-    res.render('login');
-});
 
 router.get('/logout', (req, res) => {
         req.session.destroy();
@@ -72,6 +69,11 @@ router.get('/logout', (req, res) => {
         
 });
 
+
+
+router.use(function (req, res, next) {
+  res.status(404).send("I surely hope you know what you are doing, becasue I dont!")
+});
 export default router;
 
 
